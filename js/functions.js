4351bb622ccb414e21278e8c3581a1007c8c470b
Zachary Burke <zac.burke@mig.cc>
2017-04-11 21:25:06 -0400

    Updating with working files

---
+++ js/functions.js
@@ -0,0 +1,28 @@
+console.log("TEST");
+$(document).ready(function() {
+  var showing = 0;
+  var embedcodes = ['Burkenator','BrntO_Zeke','Cduval91','Creeeeg','DigitalDisorder','EskimoBobberton','Gamer_5005','Jchugs','Megaman320','Melkor','PainlessRisk'];
+  var divArray = $('.videoDiv').get();
+  console.log(divArray.length);
+  var widthVideo = 640;
+  var heightVideo = 360;
+
+  $('input').on('change', function(){
+      var clickedNum = $( "input" ).index( this )
+      var currentDiv = $('.videoDiv').eq(clickedNum);
+	  var currentDivContent = $('.card-block').eq(clickedNum);
+	  var userName = $('h2').eq(clickedNum);
+      var stringConstructor = '<iframe class="videoFrame" src="https://beam.pro/embed/player/' + embedcodes[clickedNum] + '" style="width:' + widthVideo + ';height:' + heightVideo + ';"></iframe>';
+	  
+      if(this.checked){
+
+        currentDiv.show();
+        currentDivContent.html(stringConstructor);
+		userName.html(embedcodes[clickedNum]);
+      }
+      else{
+        currentDiv.hide();
+        currentDivContent.html("");
+      }
+    });
+});
